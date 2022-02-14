0ff6f1b60a77f8216f0944ce6978feb3332acd9a
kimseongah <kimsa0322@naver.com>
2022-02-15 02:55:06 +0900

    add share function

--- js/share.js
+++ js/share.js
@@ -1,3 +1,34 @@
+const url = 'https://seongah-mbti-test.netlify.app/';
+function setShare(){
+  var resultImg = document.querySelector('#resultImg');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = 'mbti 테스트 결과';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'img/image-'+resultAlt+'.png';
+  const shareURL = url + 'page.result-'+resultAlt+'.html';
+  Kakao.Link.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: shareTitle,
+      description: shareDes,
+      imageUrl:shareImage,
+      link: {
+        mobileWebUrl: shareURL,
+        WebUrl: shareURL
+      },
+    },
+    buttons: [
+      {
+        title: '결과확인하기',
+        link: {
+          mobileWebUrl: shareURL,
+          WebUrl: shareURL
+        },
+      },
+    ]
+  });
+}
+
 function kakaoshare(){
   Kakao.Link.sendDefault({
     objectType: 'feed',
